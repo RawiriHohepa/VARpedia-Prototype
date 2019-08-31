e44cdd26695991ab593234ee078989c6409e55bb
RawiriHohepa <rawirihohepa@gmail.com>
2019-08-31 17:16:43 +1200

    created basic GUI with buttons

--- src/wikisearch/Main.java
+++ src/wikisearch/Main.java
@@ -4,19 +4,26 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.List;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 import java.io.BufferedReader;
 
-public class Main {
+public class Main extends Application {
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
 	public static void main(String[] args) {
 		try {
 			ProcessBuilder builder = new ProcessBuilder("/bin/bash", "-c", "./script.sh p");
 			Process process = builder.start();
 			InputStream stdout = process.getInputStream();
 			InputStream stderr = process.getErrorStream();
-			BufferedReader stdoutBuffered =
-					new BufferedReader(new InputStreamReader(stdout));
+			BufferedReader stdoutBuffered = new BufferedReader(new InputStreamReader(stdout));
 			String line = null;
 			while ((line = stdoutBuffered.readLine()) != null ) {
 				System.out.println(line);
@@ -25,4 +32,6 @@ public class Main {
 			e.printStackTrace();
 		}
 	}
+
+	
 }
